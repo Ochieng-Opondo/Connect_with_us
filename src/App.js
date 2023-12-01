5af7ea90f83d77c446bb14db97529c4f2cdc8a65
Ochieng-Opondo <94851639+Ochieng-Opondo@users.noreply.github.com>
2023-12-01 13:11:00 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import Landing from './Components/Landing';
 import About from './Components/About';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import { RouterProvider } from "react-router-dom";
+import Login from './Components/login'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]);
 
 
 function App({ cardObject }) {
 
-
   return (
     <div>
-      <Header />
-      <Landing />
-      <About cardObject={cardObject} />
-      <Footer />
+    <Header />
+    < RouterProvider router = { router } />
+      <About cardObject={ cardObject } />
+        < Footer />
 
-    </div>
+        </div>
   );
 
 }
